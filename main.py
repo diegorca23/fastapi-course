b18114a2d608da2a007a79ef4f9219436199f03e
diegorca23 <diego.rca.23@gmail.com>
2024-10-23 11:56:38 -0300

    Utilizando Esquemas de Pydantic. Modificado create_movie() y update_movie()

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 app.title = "Mi aplicación con FastAPI"  # Agregando título 
 app.version = "2.3"                      # Agregando version
+
+
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str
+    overview: str
+    year: int
+    rating: float
+    category: str
 
 
 movies = [
@@ -60,31 +71,22 @@ def get_movie_by_category(category: str, year: int):
     return filtered_by_category
 
 @app.post('/movies', tags=['movies'])
-def create_movie(id: int = Body(), title: str= Body(), overview: str= Body(), year: int= Body(), rating: float= Body(), category: str= Body()):
+def create_movie(movie: Movie):
     
-    movies.append(
-        {
-        "id": id, 
-        "title": title, 
-        "overview": overview, 
-        "year": year, 
-        "rating": rating, 
-        "category": category
-        }
-    )
+    movies.append(movie)
     return movies
 
 @app.put('/movies/{id}', tags=['movies'])
-def update_movie(id: int, title: str= Body(), overview: str= Body(), year: int= Body(), rating: float= Body(), category: str= Body()):
+def update_movie(id: int, movie:Movie):
     
 
     for item in movies:
         if item["id"]==id:
-            item["title"]: title
-            item["overview"]: overview
-            item["year"]: year
-            item["rating"]: rating
-            item["category"]: category
+            item["title"]: movie.title
+            item["overview"]: movie.overview
+            item["year"]: movie.year
+            item["rating"]: movie.rating
+            item["category"]: movie.category
             return movies
 
 
